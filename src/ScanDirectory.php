05b10a9e5a24e109690f91e870905ea8b78f3342
Alexandre Buleté <bulete.alexandre@gmail.com>
2021-04-06 16:54:26 +0200

    fix: unlink condition

--- src/ScanDirectory.php
+++ src/ScanDirectory.php
@@ -34,16 +34,10 @@ class ScanDirectory {
             $created_at = filectime($this->files[$i]);
             $now = getdate()[0];
             $this->{$this->action}();
-            // echo '<pre>'; var_dump($now - $created_at, $this->files, $_SERVER['DOCUMENT_ROOT']); echo '</pre>';
-            // echo '<pre>'; var_dump(getdate(), $this->files[$i]); echo '</pre>';
-            // echo $this->files[$i];
 
-            // if ($now - $created_at > self::ONE_DAY_IN_SECONDS * 30) {
             if ($now - $created_at > self::ONE_DAY_IN_SECONDS * 30) {
-                file_exists($this->file[$i]) ? unlink($this->files[$i]) : '';
+                file_exists($this->files[$i]) ?  unlink($this->files[$i]) : '';
             }
-            // echo '<pre>'; var_dump(strtotime('now'), strtotime("now -30 days") + 86400 * 30); echo '</pre>';
-            // echo '<pre>'; var_dump(strtotime('-30 days', $created_at), strtotime("now -30 days") + 86400 * 30); echo '</pre>';
         }
         return $this;
     }
